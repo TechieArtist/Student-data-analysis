630bf56d02613ec89e3081b953022b7dfc524976
Evan <106931706+TechieArtist@users.noreply.github.com>
2024-08-06 19:31:19 +0900

    Update model_training.py

--- source/model_training.py
+++ source/model_training.py
@@ -1,8 +1,3 @@
-import sys
-import os
-
-# Add the project root directory to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import torch
 from sklearn.model_selection import train_test_split, cross_val_score
