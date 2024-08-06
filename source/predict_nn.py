9ed72d7583260bbfb81f493c8e3ac4942092c29c
Evan <106931706+TechieArtist@users.noreply.github.com>
2024-08-06 19:31:27 +0900

    Update predict_nn.py

--- source/predict_nn.py
+++ source/predict_nn.py
@@ -1,8 +1,3 @@
-import sys
-import os
-
-# Add the project root directory to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import numpy as np
 import joblib
